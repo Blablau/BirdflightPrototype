3bb94196992bfbf8cbfc5f5a7477fcde05fe17ff
Fabian <fabian.stiedl@gmail.com>
2017-04-17 12:14:15 +0200

    setting up

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public GameObject target;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    // More preferred Update-Method for Cameras (gets called after Player calculation)
+    void LateUpdate()
+    {
+        transform.LookAt(target.transform);
+    }
+}
